ffa7e9cf4bb598818d986a0822df4777b4e23e3e
santanu.patra <santanu.patra@webskitters.com>
2025-07-16 11:52:49 +0530

    more modications

--- functions.php
+++ functions.php
@@ -8,38 +8,28 @@ function hello_elementor_child_enqueue_scripts() {
 		],
 		rand(1, 10)
 	);
-}
-add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts' );
-
-//-------Custom Js file----------------
-function mycustomscript_enqueue() {
+	
     wp_enqueue_script( 'custom-scripts', get_stylesheet_directory_uri() . '/script.js', array( 'jquery' ), true, true);
 
     // wp_enqueue_script('fancybox', '//cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js', ['jquery'], '3.5.7', true);
     // wp_enqueue_style('fancyBox-css','//cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css', [], '3.5.7', 'all');
 	
-	wp_enqueue_script('owlcarouseljs', '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js', ['jquery'], '2.3.4', true);
-    wp_enqueue_style('owlcarouselcss','//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css', [], '2.3.4', 'all');
-	wp_enqueue_style('owlcarouselthemecss','//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css', [], '2.3.4', 'all');
+	// wp_enqueue_script('owlcarouseljs', '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js', ['jquery'], '2.3.4', true);
+    // wp_enqueue_style('owlcarouselcss','//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css', [], '2.3.4', 'all');
+	// wp_enqueue_style('owlcarouselthemecss','//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css', [], '2.3.4', 'all');
 	
 	// wp_enqueue_style('animatecss','//cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css', [], '3.5.2', 'all');
 }
-add_action( 'wp_enqueue_scripts', 'mycustomscript_enqueue' );
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts' );
 //-----------------------
 
 
 
 //Below codes helps to create a dynamic copyrights line footer
-function year_shortcode() {
-  	$year = date('Y');
-  	return $year;
-}
-add_shortcode('year', 'year_shortcode');
-
 function site_title_shortcode() {
   	$site_title = get_bloginfo( 'name' );
 	$site_url = site_url();
-	$title_url = '<a href="'.$site_url.'" style="font-size: 15px; color: #fff;">'.$site_title.'</a>';
+	$title_url = '<a href="'.$site_url.'">'.$site_title.'</a>';
   	return $title_url;
 }
 add_shortcode('site_title', 'site_title_shortcode');
